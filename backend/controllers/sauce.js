48118a0ccd990b7e668a7fe8739a235e5df858db
Yanis <yanis.l69520@live.fr>
2021-10-23 12:01:53 +0200

    Ajouter sauce

---
+++ backend/controllers/sauce.js
@@ -0,0 +1,13 @@
+const Sauce = require("../models/Sauce");
+
+exports.createSauce = (req, res, next) => {
+    const sauceObject = JSON.parse(req.body.sauce);
+    delete sauceObject._id;
+    const sauce = new Sauce({
+        ...sauceObject,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    });
+    sauce.save()
+        .then(() => res.status(201).json({ message: 'Sauce enregistré !'}))
+        .catch(error => res.status(400).json({ error }));
+};
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
+const sauceCtrl = require("../controllers/sauce");
 
 router.get('/', auth);
-router.post('/', auth, multer);
+router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/:id', auth);
 router.put('/:id', auth, multer);
 router.delete('/:id', auth);
