3b23879ff8ebb6f9c818af55640cf08510aee044
Yanis <yanis.l69520@live.fr>
2021-10-24 10:21:18 +0200

    modifier une sauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -43,6 +43,17 @@ exports.getAllSauces = (req, res, next) => {
     );
   };
 
+  exports.modifySauce = (req, res, next) => {
+    const sauceObject = req.file ?
+      {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+      } : { ...req.body };
+    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+  };
+
   exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
       .then(sauce => {
